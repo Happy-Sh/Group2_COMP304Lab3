a723be2a9c98bceb9c58a825fd22cee375cd0aa5
Happy-S <118223124+Happy-Shrestha@users.noreply.github.com>
2023-06-22 14:01:21 -0400

    create main activity

---
+++ app/src/main/java/com/group2_comp304lab3/Exercise1.kt
@@ -0,0 +1,14 @@
+package com.group2_comp304lab3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+
+class Exercise1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise1)
+    }
+}
---
+++ app/src/main/java/com/group2_comp304lab3/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.group2_comp304lab3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val ex1 = findViewById<TextView>(R.id.exercise1TextView)
+        val ex2 = findViewById<TextView>(R.id.exercise2TextView)
+        val ex3 = findViewById<TextView>(R.id.exercise3TextView)
+
+        ex1.setOnClickListener{
+            val intent = Intent(this,Exercise1 ::class.java)
+            startActivity(intent)
+        }
+        ex2.setOnClickListener{
+            val intent = Intent(this,Exercise2 ::class.java)
+            startActivity(intent)
+        }
+        ex3.setOnClickListener{
+            val intent = Intent(this,Exercise2 ::class.java)
+            startActivity(intent)
+        }
+    }
+}
